edd57101914b99b6105e33247b0c74f0935c335c
Daniel Boyer <danielkboyer@gmail.com>
2024-07-25 13:12:17 -0600

    Time: 73 ms (49.36%) | Memory: 54 MB (65.04%) - LeetSync

--- 135-candy/candy.ts
+++ 135-candy/candy.ts
@@ -1,48 +1,31 @@
 function candy(ratings: number[]): number {
 
     var n = ratings.length;
-    var ratingWithIndex = Array(n);
-
-    for(var i =0;i<n;i++){
-    ratingWithIndex[i] = new RatingWithIndex(ratings[i],i);
-
-    }
-
-
     var candies = Array(n).fill(1);
+    for(var i =1;i<n;i++){
+
+        var leftRating = ratings[i-1];
+        var rating = ratings[i];
+        if(rating > leftRating){
+            if(candies[i] <= candies[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+    }
 
     var totalCandy = 0;
 
-    ratingWithIndex.sort((a,b)=>a.rating - b.rating);
-    
-    for(var i =0;i<n;i++){
-        var ratingAndIndex = ratingWithIndex[i];
-        var index = ratingAndIndex.index;
-        var rating = ratingAndIndex.rating;
-
-        var leftRating = index > 0 ? ratings[index-1]: ratings[0];
-        var rightRating = index < n-1 ? ratings[index+1]: ratings[index];
-
-
-        if(rating > leftRating){
-            if(candies[index] <= candies[index-1]){
-                candies[index] = candies[index-1] + 1;
-            }
-        }
+    for(var i = n-1;i>=0;i--){
+         var rightRating = ratings[i+1];
+        var rating = ratings[i];
 
         if(rating > rightRating){
-            if(candies[index] <= candies[index+1]){
-                candies[index] = candies[index+1] +1;
+            if(candies[i] <= candies[i+1]){
+                candies[i] = candies[i+1] + 1;
             }
         }
-
-        totalCandy += candies[index];
+        totalCandy+=candies[i];
     }
 
     return totalCandy;
 };
-
-
-class RatingWithIndex{
-    constructor(public rating, public index){}
-}
